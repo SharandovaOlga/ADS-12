a1d01cadcdf6c26900cb2979c5dcca099795cbad
SharandovaOlga <75723905+SharandovaOlga@users.noreply.github.com>
2022-04-17 22:06:02 +0300

    ADS-6 Sharandova

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,21 @@
 template<typename T, int size>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+  T arr[size] = { 0 };
+  int l, f;
+ public:
+  void push(T x) {
+    int j = l;
+    for ( ; (j >= f) && arr[j % size].prior < x.prior; --j) {
+      arr[(j + 1) % size] = arr[j % size];
+    }
+    arr[(j + 1) % size] = x;
+    ++l;
+  }
+  T pop() {
+    return arr[(f++) % size];
+  }
 };
 
 struct SYM {
